a75c16071364f4ff114c572764597236d546e8d2
CoderfishGzh <335247945@qq.com>
2022-05-20 23:28:11 +0800

    实现修改版 User_kvminit()函数

--- kernel/vm.c
+++ kernel/vm.c
@@ -47,6 +47,43 @@ kvminit()
   kvmmap(TRAMPOLINE, (uint64)trampoline, PGSIZE, PTE_R | PTE_X);
 }
 
+void
+User_kvmmap(pagetable_t page, uint64 va, uint64 pa, uint64 sz, int perm)
+{
+  if(mappages(page, va, sz, pa, perm) != 0)
+    panic("kvmmap");
+}
+
+pagetable_t
+User_kvminit() {
+  pagetable_t page = (pagetable_t) kalloc();
+  memset(kernel_pagetable, 0, PGSIZE);
+
+  // uart registers
+  User_kvmmap(page, UART0, UART0, PGSIZE, PTE_R | PTE_W);
+
+  // virtio mmio disk interface
+  User_kvmmap(page ,VIRTIO0, VIRTIO0, PGSIZE, PTE_R | PTE_W);
+
+  // CLINT
+  // User_kvmmap(page, CLINT, CLINT, 0x10000, PTE_R | PTE_W);
+
+  // PLIC
+  User_kvmmap(page, PLIC, PLIC, 0x400000, PTE_R | PTE_W);
+
+  // map kernel text executable and read-only.
+  User_kvmmap(page, KERNBASE, KERNBASE, (uint64)etext-KERNBASE, PTE_R | PTE_X);
+
+  // map kernel data and the physical RAM we'll make use of.
+  User_kvmmap(page, (uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);
+
+  // map the trampoline for trap entry/exit to
+  // the highest virtual address in the kernel.
+  User_kvmmap(page, TRAMPOLINE, (uint64)trampoline, PGSIZE, PTE_R | PTE_X);
+
+  return page;
+}
+
 // Switch h/w page table register to the kernel's page table,
 // and enable paging.
 void
