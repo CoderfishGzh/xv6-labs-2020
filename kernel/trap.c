e48cdf30533416a9e9799b159b60da5d0e92825e
CoderfishGzh <335247945@qq.com>
2023-01-19 00:55:07 +0800

    change

--- kernel/trap.c
+++ kernel/trap.c
@@ -95,7 +95,7 @@ usertrap(void)
       p->interval_from_prev_alarm++;
 
       // 保存相关的寄存器
-      memmove(p->trapframe, p->alarm_trapframe, PGSIZE);
+      memmove(p->alarm_trapframe, p->trapframe, PGSIZE);
 
       if(p->interval_from_prev_alarm >= p->alarm_interval && p->handler_exec == 0) {
         p->handler_exec = 1;
